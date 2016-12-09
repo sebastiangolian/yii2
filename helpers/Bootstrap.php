3e245e61ec5cdc727962790068797c77a1f72a94
Sebastian Golian <Sebastian Golian>
2016-12-09 19:13:52 +0100

    test

--- helpers/Bootstrap.php
+++ helpers/Bootstrap.php
@@ -1,17 +1,10 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace sebastiangolian\yii2\helpers;
 
 /**
  * Description of Bootstrap
  *
- * @author Sammy Guergachi <sguergachi at gmail.com>
+ * @author Sebastian Golian <sebastiangolian@gmail.com>
  */
 class Bootstrap {
     //put your code here
