de50c076e1b37712a0eb2d147a96993e02015435
GolianSebastian <GolianSebastian@net.pp>
2016-12-15 14:45:50 +0100

    test

--- helpers/Bootstrap.php
+++ helpers/Bootstrap.php
@@ -7,9 +7,30 @@ namespace sebastiangolian\yii2\helpers;
  * @author Sebastian Golian <sebastiangolian@gmail.com>
  */
 class Bootstrap {
-//test
-    public static function test()
+     /**
+     * Generates a bootstrap icon markup.
+     *
+     * Example:
+     *
+     * ~~~
+     * echo Html::icon('pencil');
+     * echo Html::icon('trash', ['style' => 'color: red; font-size: 2em']);
+     * echo Html::icon('plus', ['class' => 'text-success']);
+     * ~~~
+     *
+     * @see http://getbootstrap.com/components/#glyphicons
+     *
+     * @param string $icon the bootstrap icon name without prefix (e.g. 'plus', 'pencil', 'trash')
+     * @param array $options HTML attributes / options for the icon container
+     * @param string $prefix the css class prefix - defaults to 'glyphicon glyphicon-'
+     * @param string $tag the icon container tag (usually 'span' or 'i') - defaults to 'span'
+     *
+     * @return string
+     */
+    public static function icon($icon, $options = [], $prefix = 'glyphicon glyphicon-', $tag = 'span')
     {
-        return 'test';
+        $class = isset($options['class']) ? ' ' . $options['class'] : '';
+        $options['class'] = $prefix . $icon . $class;
+        return static::tag($tag, '', $options);
     }
 }
